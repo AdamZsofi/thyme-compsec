92d7f6e09c7f28bc2933cb9a719a2a9ccca84620
Pintér Balázs <37505930+pinterbalazs21@users.noreply.github.com>
2022-11-26 22:36:17 +0100

    init2

--- server/src/main/java/hu/bme/crysys/server/server/domain/parser/CaffParseResult.java
+++ server/src/main/java/hu/bme/crysys/server/server/domain/parser/CaffParseResult.java
@@ -7,18 +7,21 @@ public class CaffParseResult {
 //CAFF description: https://www.crysys.hu/downloads/vihima06/2020/CAFF.txt
 //CIFF descripton: https://www.crysys.hu/downloads/vihima06/2020/CIFF.txt
 
-private int numAnim;
-private String date;
-private List<CIFF> ciffList;
-
+public int numAnim;
+public String date;
+public List<CIFF> ciffList = new ArrayList();
 
 public static class CIFF{
-    private int duration;
-    private int width;
-    private int height;
-    private String caption;
-    private List<String> tags;
-    private String fileName;
+    public CIFF(String fileName, int duration, String caption, ArrayList<String> tags){
+        this.fileName = fileName;
+        this.duration = duration;
+        this.caption = caption;
+        this.tags = tags;
+    }
+    public final String fileName;
+    public final int duration;
+    public final String caption;
+    public final List<String> tags;
 }
 
 }
--- server/src/main/java/hu/bme/crysys/server/server/domain/parser/ParserController.java
+++ server/src/main/java/hu/bme/crysys/server/server/domain/parser/ParserController.java
@@ -1,10 +1,49 @@
 package hu.bme.crysys.server.server.domain.parser;
+import org.json.*;
 
 public class ParserController {
 
-public bool parse(string CAFFFileName, string outputPath){
+String parserPath = "parserpath";//todo provide the path of your compiled CAFF parser.
+
+public CaffParseResult parse(string CAFFFileName, string outputPath){
     Runtime rt = Runtime.getRuntime();
     Process pr = rt.exec("stuff "  + CAFFFileName + " " + outputPath);
+
+    CaffParseResult result = new CaffParseResult();
+    //maybe current wd + outputpath or something else? 
+    final File outputFolder = new File(outputPath); 
+    scanAndSaveMetaData(folder);
+
 }
+
+private void scanAndSaveMetaData(final File folder) {
+    for (final File file : folder.listFiles()) {
+        if (file.getName()) {
+            CaffParseResult.CIFF ciff = jsonToCIFF(file.getPath());
+        }
+    }
+}
+
+Boolean isJson(String filename){
+    ArrayList fileSplit = filename.split(".");
+    return fileSplit.get(fileSplit.size() - 1) == "json";
+}
+
+CaffParseResult.CIFF jsonToCIFF(Path path){
+    String jsonfilename = path.getFileName();
+    String imgFileName = jsonfilename.replaceAll("json", "bmp");
+    String jsonString = Files.readString(path, encoding);
+    JSONObject obj = new JSONObject(jsonString);
+    Integer duration = obj.getInt("duration");
+    String caption = obj.getString("caption");
+    JSONArray tagsArr = obj.getJSONArray("tags");
+    ArrayList<String> tagsArrList = new ArrayList<String>();
+    for (int i = 0; i < tagsArr.length(); i++){
+        String tag = arr.getJsonString(i);
+        tagsArrList.add(tag);
+    }
+    return CaffParseResult.CIFF( , duration, caption, tagsArrList);
+}
+
 
 }
