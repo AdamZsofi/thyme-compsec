b158389cc6546d51272768918919abb73c67e53f
AdamZsofi <adamzsofi.mail@gmail.com>
2022-12-01 11:56:50 +0100

    cert script typo fix

--- util/certi-gen.sh
+++ util/certi-gen.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-mkdir -p certs
+mkdir -p frontend-certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout frontend-certs/selfsigned.key -out frontend-certs/selfsigned.crt
+mkdir -p backend-certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout backend-certs/selfsigned.key -out backend-certs/selfsigned.crt
