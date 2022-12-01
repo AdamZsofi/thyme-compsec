9ec6cbf10b578d6c047cc517e2742add7e9b6421
Gergely Ulicska <ulicskagergo@gmail.com>
2022-12-01 11:18:17 +0100

    begin transition to userdetails

--- server/src/main/java/hu/bme/crysys/server/server/controller/UserController.java
+++ server/src/main/java/hu/bme/crysys/server/server/controller/UserController.java
@@ -3,6 +3,7 @@ package hu.bme.crysys.server.server.controller;
 import java.util.Objects;
 
 import hu.bme.crysys.server.server.domain.database.UserData;
+import hu.bme.crysys.server.server.domain.security.PasswordConfiguration;
 import hu.bme.crysys.server.server.repository.UserDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,15 +12,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import static hu.bme.crysys.server.server.domain.security.ApplicationUserRole.USER;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    //@Autowired
+    //private UserDataRepository userDataRepository;
+
     @Autowired
-    private UserDataRepository userDataRepository;
+    private InMemoryUserDetailsManager inMemoryUserDetailsManager;
+
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
 
     @RequestMapping(value = {"/ami_admin"}, method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> amiAdmin() {
@@ -56,16 +69,21 @@ public class UserController {
     @GetMapping(value = "/register",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserData> registerUser(@RequestBody @Validated UserData userData) {
+    public ResponseEntity<?> registerUser(@RequestBody @Validated UserData userData) {
         // TODO Check if user already logged in! Should send an error back if user is logged in already
         // TODO check password requirements here as well (already checked once on client side, but needs to be checked here as well)
         // requirements: minLength: 10, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1
         String password = userData.getPassword();
         String username = userData.getUserName();
         if (password != null && username != null) {
-            UserData existingUserData = userDataRepository.findUserDataByUsername(username);
-            if (existingUserData == null) {
-                return new ResponseEntity<>(userDataRepository.saveAndFlush(userData), HttpStatus.OK);
+            if (!inMemoryUserDetailsManager.userExists(username)) {
+                UserDetails userDetails = User
+                        .withUsername(username)
+                        .password(passwordEncoder.encode(password))
+                        .roles(USER.name())
+                        .build();
+                inMemoryUserDetailsManager.createUser(userDetails);
+                return new ResponseEntity<>(HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
@@ -74,7 +92,7 @@ public class UserController {
         }
     }
 
-    @GetMapping(value = "/login",
+    /*@GetMapping(value = "/login",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> loginUser(@RequestBody @Validated UserData userData) {
@@ -91,5 +109,5 @@ public class UserController {
         } else {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-    }
+    }*/
 }
