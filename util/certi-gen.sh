1f360f1f9e6fefd9a8193db1cba72f36f7fc7a49
AdamZsofi <adamzsofi.mail@gmail.com>
2022-12-01 11:53:46 +0100

    Modified cert gen to generate 2 certs

--- util/certi-gen.sh
+++ util/certi-gen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p certs
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/selfsigned.key -out certs/selfsigned.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout frontend-certs/selfsigned.key -out frontend-certs/selfsigned.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout backend-certs/selfsigned.key -out backend-certs/selfsigned.crt
